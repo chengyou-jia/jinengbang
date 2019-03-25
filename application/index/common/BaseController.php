05d408149508d77137772699e67b0f01ff13e956
Jiachengyou <jiachengyou@tiaozhan.com>
2019-03-25 10:34:54 +0800

    更改基本框架 包括common和index模块

---
+++ application/index/common/BaseController.php
@@ -0,0 +1,18 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * Author: 贾成铕
+ * Email: jiachengyou@tiaozhan.com
+ * Date: 2019/3/25
+ * Time: 10:30
+ */
+
+namespace app\index\common;
+
+
+use think\Controller;
+
+class BaseController extends Controller
+{
+
+}
---
+++ application/index/common/BaseValidate.php
@@ -0,0 +1,18 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * Author: 贾成铕
+ * Email: jiachengyou@tiaozhan.com
+ * Date: 2019/3/25
+ * Time: 10:31
+ */
+
+namespace app\index\common;
+
+
+use think\Validate;
+
+class BaseValidate extends Validate
+{
+
+}
